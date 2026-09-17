936e75b5226b789bf51e8e3cfd755d5dc067ce17
Brent Warner <brent.stanson@gmail.com>
2026-09-17 14:52:00 -0400

    Trace packaged server runtime imports

--- apps/server/src/runtime.ts
+++ apps/server/src/runtime.ts
@@ -1,16 +1,57 @@
-import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
-import * as NodeServices from "@effect/platform-node/NodeServices";
-import { NetService } from "@graft/shared/Net";
-import * as Effect from "effect/Effect";
-import { FetchHttpClient } from "effect/unstable/http";
-import { Command } from "effect/unstable/cli";
-import * as Layer from "effect/Layer";
+import type * as EffectType from "effect/Effect";
 
 import { version } from "../package.json" with { type: "json" };
-import { consumeDesktopParentInput, withDesktopParentLifetime } from "./desktopParentLifetime";
-import { ServerLive } from "./effectServer";
-import { CliConfig, graftCli } from "./main";
-import { OpenLive } from "./open";
+
+const tracePackagedImport = (message: string): void => {
+  if (process.env.GRAFT_DESKTOP_PACKAGED === "1") {
+    process.stderr.write(`[server] runtime import ${message}\n`);
+  }
+};
+
+tracePackagedImport("@effect/platform-node/NodeRuntime started");
+const NodeRuntime = await import("@effect/platform-node/NodeRuntime");
+tracePackagedImport("@effect/platform-node/NodeRuntime finished");
+
+tracePackagedImport("@effect/platform-node/NodeServices started");
+const NodeServices = await import("@effect/platform-node/NodeServices");
+tracePackagedImport("@effect/platform-node/NodeServices finished");
+
+tracePackagedImport("@graft/shared/Net started");
+const { NetService } = await import("@graft/shared/Net");
+tracePackagedImport("@graft/shared/Net finished");
+
+tracePackagedImport("effect/Effect started");
+const Effect = await import("effect/Effect");
+tracePackagedImport("effect/Effect finished");
+
+tracePackagedImport("effect/unstable/http started");
+const { FetchHttpClient } = await import("effect/unstable/http");
+tracePackagedImport("effect/unstable/http finished");
+
+tracePackagedImport("effect/unstable/cli started");
+const { Command } = await import("effect/unstable/cli");
+tracePackagedImport("effect/unstable/cli finished");
+
+tracePackagedImport("effect/Layer started");
+const Layer = await import("effect/Layer");
+tracePackagedImport("effect/Layer finished");
+
+tracePackagedImport("desktopParentLifetime started");
+const { consumeDesktopParentInput, withDesktopParentLifetime } =
+  await import("./desktopParentLifetime");
+tracePackagedImport("desktopParentLifetime finished");
+
+tracePackagedImport("effectServer started");
+const { ServerLive } = await import("./effectServer");
+tracePackagedImport("effectServer finished");
+
+tracePackagedImport("main started");
+const { CliConfig, graftCli } = await import("./main");
+tracePackagedImport("main finished");
+
+tracePackagedImport("open started");
+const { OpenLive } = await import("./open");
+tracePackagedImport("open finished");
 
 const desktopParentInput = consumeDesktopParentInput(process.env, () => process.stdin);
 
@@ -26,4 +67,4 @@ const RuntimeLayer = Layer.empty.pipe(
 Command.run(graftCli, { version })
   .pipe(Effect.provide(RuntimeLayer))
   .pipe((program) => withDesktopParentLifetime(program, desktopParentInput))
-  .pipe((program) => NodeRuntime.runMain(program as Effect.Effect<void, unknown, never>));
+  .pipe((program) => NodeRuntime.runMain(program as EffectType.Effect<void, unknown, never>));
